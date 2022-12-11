7c0870b07208f707b09e5d7d08acbbe1e6d0cb43
renosyah <syahputrareno975@gmail.com>
2022-12-11 20:52:23 +0700

    fix sql dump file

--- sql/web_game_db.sql
+++ sql/web_game_db.sql
@@ -1,89 +1,26 @@
--- MariaDB dump 10.19  Distrib 10.10.2-MariaDB, for Win64 (AMD64)
---
--- Host: localhost    Database: web_game_db
--- ------------------------------------------------------
--- Server version	10.10.2-MariaDB
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `game`
---
-
-DROP TABLE IF EXISTS `game`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `game` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `game_name` text DEFAULT NULL,
-  `game_description` text DEFAULT NULL,
-  `game_url` text DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE game(
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_name TEXT,
+    game_description TEXT,
+    game_url TEXT
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `game`
---
+CREATE TABLE scoreboard(
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_id INT(11) NOT NULL,
+    player_id TEXT,
+    player_name TEXT,
+    score INT(11),
+    FOREIGN KEY (game_id) REFERENCES game(id)
+);
 
-LOCK TABLES `game` WRITE;
-/*!40000 ALTER TABLE `game` DISABLE KEYS */;
-INSERT INTO `game` VALUES
+INSERT INTO game VALUES
 (4,'Balloon Pop','game about popping balloon','/games/balloon-pop/'),
 (5,'Gold fish','game gold fish collecting gold coin','/games/gold-fish/'),
 (6,'Tank tab','game about tank tab and shooting','/games/tank-tab/');
-/*!40000 ALTER TABLE `game` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `scoreboard`
---
-
-DROP TABLE IF EXISTS `scoreboard`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `scoreboard` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `game_id` int(11) NOT NULL,
-  `player_id` text DEFAULT NULL,
-  `player_name` text DEFAULT NULL,
-  `score` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `game_id` (`game_id`),
-  CONSTRAINT `scoreboard_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)
-);
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `scoreboard`
---
-
-LOCK TABLES `scoreboard` WRITE;
-/*!40000 ALTER TABLE `scoreboard` DISABLE KEYS */;
-INSERT INTO `scoreboard` VALUES
+INSERT INTO scoreboard VALUES
 (11,4,'admin001','admin',105),
 (12,5,'admin001','admin',30),
 (17,6,'random_621.603341','Guest',9),
 (18,6,'admin001','admin',5);
-/*!40000 ALTER TABLE `scoreboard` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-12-11 15:45:42
