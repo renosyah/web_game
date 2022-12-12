99940d52890a6576b903e9d17acd67bca0901e0c
renosyah <syahputrareno975@gmail.com>
2022-12-12 11:31:35 +0700

    not use secure module sql

--- model/game.php
+++ model/game.php
@@ -21,42 +21,37 @@ class game {
     public function add($db) {
         $result_query = new result_query();
         $result_query->data = "ok";
-        $query = "INSERT INTO game (game_name, game_description, game_url) VALUES (?,?,?)";
-        $stmt = $db->prepare($query);
-        $stmt->bind_param('sss', $this->game_name, $this->game_description, $this->game_url);
-        $stmt->execute();
-        if ($stmt->error != ""){
-            $result_query->error =  "error at add new game : ".$stmt->error;
+        $query = "INSERT INTO game (game_name, game_description, game_url) VALUES ($this->game_name, $this->game_description, $this->game_url)";
+        $stmt = $db->query($query);
+        if (!$stmt){
+            $result_query->error = "error at add new game : ".$stmt->error;
             $result_query->data = "not ok";
+            $db->close();
         }
-        $stmt->close();
+
+        $db->close();
         return $result_query;
     }
     
     public function one($db) {
         $result_query = new result_query();
         $one = new game();
-        $query = "SELECT id, game_name, game_description, game_url FROM game WHERE id=? LIMIT 1";
-        $stmt = $db->prepare($query);
-        $stmt->bind_param('i', $this->id);
-        $stmt->execute();      
-        if ($stmt->error != ""){
-            $result_query-> error = "error at query one game : ".$stmt->error;
-            $stmt->close();
+        $query = "SELECT id, game_name, game_description, game_url FROM game WHERE id=$this->id LIMIT 1";
+        $stmt = $db->query($query);
+        if (!$stmt){
+            $result_query->error = "error at query one game : ".$stmt->error;
+            $db->close();
             return $result_query;
         }
-        $rows = $stmt->get_result();
-        if($rows->num_rows == 0){
-            $stmt->close();
-            return $result_query;
-        }
-        $result = $rows->fetch_assoc();
+
+        $result = mysqli_fetch_assoc($stmt);
         $one->id = $result['id'];
         $one->game_name = $result['game_name'];
         $one->game_description = $result['game_description'];
         $one->game_url = $result['game_url'];
         $result_query->data = $one;
-        $stmt->close();
+
+        $db->close();
         return $result_query;
     }
 
@@ -68,30 +63,25 @@ class game {
                 FROM 
                     game
                 WHERE
-                    ".$list_query->search_by." LIKE ?
+                    ".$list_query->search_by." LIKE '%".$list_query->search_value."%'
                 ORDER BY
                     ".$list_query->order_by." ".$list_query->order_dir." 
-                LIMIT ? 
-                OFFSET ?";
-        $stmt = $db->prepare($query);
-        $search = "%".$list_query->search_value."%";
-        $offset = $list_query->offset;
-        $limit =  $list_query->limit;
-        $stmt->bind_param('sii',$search ,$limit, $offset);
-        $stmt->execute();
-        if ($stmt->error != ""){
-            $result_query-> error = "error at query all game : ".$stmt->error;
-            $stmt->close();
+                LIMIT $list_query->limit OFFSET $list_query->offset";
+
+        $stmt = $db->query($query);
+        if (!$stmt){
+            $result_query->error = "error at query one game : ".$stmt->error;
+            $db->close();
             return $result_query;
         }
-        $rows = $stmt->get_result();
-        if($rows->num_rows == 0){
-            $stmt->close();
+
+        if($stmt->num_rows == 0){
+            $db->close();
             $result_query->data = $all;
             return $result_query;
         }
 
-        while ($result = $rows->fetch_assoc()){
+        while ($result = $stmt->fetch_array()){
             $one = new game();
             $one->id = $result['id'];
             $one->game_name = $result['game_name'];
@@ -99,42 +89,40 @@ class game {
             $one->game_url = $result['game_url'];
             array_push($all,$one);
         }
+
         $result_query->data = $all;
-        $stmt->close();
+
+        $db->close();
         return $result_query;
     }
 
     public function update($db) {
         $result_query = new result_query();
         $result_query->data = "ok";
-        $query = "UPDATE game SET game_name = ?, game_description = ?, game_url = ? WHERE id = ?";
-        $stmt = $db->prepare($query);
-        $stmt->bind_param('sssi', $this->game_name, $this->game_description, $this->game_url, $this->id);
-        $stmt->execute();
-        if ($stmt->error != ""){
-            $result_query->error = "error at update one game : ".$stmt->error;
+        $query = "UPDATE game SET game_name = $this->game_name, game_description = $this->game_description, game_url = $this->game_url WHERE id = $this->id";
+        $stmt = $db->query($query);
+        if (!$stmt){
+            $result_query->error = "error at update game : ".$stmt->error;
             $result_query->data = "not ok";
-            $stmt->close();
-            return $result_query;
+            $db->close();
         }
-        $stmt->close();
+
+        $db->close();
         return $result_query;
     }
     
     public function delete($db) {
         $result_query = new result_query();
         $result_query->data = "ok";
-        $query = "DELETE FROM game WHERE id=?";
-        $stmt = $db->prepare($query);
-        $stmt->bind_param('i', $this->id);
-        $stmt->execute();
-        if ($stmt->error != ""){
-            $result_query->error = "error at delete one game : ".$stmt->error;
+        $query = "DELETE FROM game WHERE id = $this->id";
+        $stmt = $db->query($query);
+        if (!$stmt){
+            $result_query->error = "error at update game : ".$stmt->error;
             $result_query->data = "not ok";
-            $stmt->close();
-            return $result_query;
+            $db->close();
         }
-        $stmt->close();
+
+        $db->close();
         return $result_query;
     }
 }
--- model/score.php
+++ model/score.php
@@ -23,43 +23,37 @@ class score {
     public function add($db) {
         $result_query = new result_query();
         $result_query->data = "ok";
-        $query = "INSERT INTO scoreboard (game_id, player_id, player_name, score) VALUES (?, ?, ?, ?)";
-        $stmt = $db->prepare($query);
-        $stmt->bind_param('issi', $this->game_id, $this->player_id, $this->player_name, $this->score);
-        $stmt->execute();
-        if ($stmt->error != ""){
-            $result_query->error =  "error at add new score : ".$stmt->error;
+        $query = "INSERT INTO scoreboard (game_id, player_id, player_name, score) VALUES ($this->game_id, $this->player_id, $this->player_name, $this->score)";
+        $stmt = $db->query($query);
+        if (!$stmt){
+            $result_query->error = "error at add new score : ".$stmt->error;
             $result_query->data = "not ok";
+            $db->close();
         }
-        $stmt->close();
+
+        $db->close();
         return $result_query;
     }
     
     public function one($db) {
         $result_query = new result_query();
         $one = new score();
-        $query = "SELECT id, game_id, player_id, player_name, score FROM scoreboard WHERE id=? LIMIT 1";
-        $stmt = $db->prepare($query);
-        $stmt->bind_param('i', $this->id);
-        $stmt->execute();      
-        if ($stmt->error != ""){
-            $result_query-> error = "error at query one score : ".$stmt->error;
-            $stmt->close();
+        $query = "SELECT id, game_id, player_id, player_name, score FROM scoreboard WHERE id=$this->id LIMIT 1";
+        $stmt = $db->query($query);
+        if (!$stmt){
+            $result_query->error = "error at query one game : ".$stmt->error;
+            $db->close();
             return $result_query;
         }
-        $rows = $stmt->get_result();
-        if($rows->num_rows == 0){
-            $stmt->close();
-            return $result_query;
-        }
-        $result = $rows->fetch_assoc();
+
+        $result = mysqli_fetch_assoc($stmt);
         $one->id = $result['id'];
         $one->game_id = $result['game_id'];
         $one->player_id = $result['player_id'];
         $one->player_name = $result['player_name'];
         $one->score = $result['score'];
         $result_query->data = $one;
-        $stmt->close();
+        $db->close();
         return $result_query;
     }
 
@@ -99,30 +93,25 @@ class score {
                 FROM 
                     scoreboard
                 WHERE
-                    ".$list_query->search_by." LIKE ?
+                    ".$list_query->search_by." LIKE '%".$list_query->search_value."%'
                 ORDER BY
                     ".$list_query->order_by." ".$list_query->order_dir." 
-                LIMIT ? 
-                OFFSET ?";
-        $stmt = $db->prepare($query);
-        $search = "%".$list_query->search_value."%";
-        $offset = $list_query->offset;
-        $limit =  $list_query->limit;
-        $stmt->bind_param('sii',$search ,$limit, $offset);
-        $stmt->execute();
-        if ($stmt->error != ""){
-            $result_query-> error = "error at query all score : ".$stmt->error;
-            $stmt->close();
+                LIMIT $list_query->limit OFFSET $list_query->offset";
+
+        $stmt = $db->query($query);
+        if (!$stmt){
+            $result_query->error = "error at query one score : ".$stmt->error;
+            $db->close();
             return $result_query;
         }
-        $rows = $stmt->get_result();
-        if($rows->num_rows == 0){
-            $stmt->close();
+
+        if($stmt->num_rows == 0){
+            $db->close();
             $result_query->data = $all;
             return $result_query;
         }
 
-        while ($result = $rows->fetch_assoc()){
+        while ($result = $stmt->fetch_array()){
             $one = new score();
             $one->id = $result['id'];
             $one->game_id = $result['game_id'];
@@ -132,41 +121,38 @@ class score {
             array_push($all,$one);
         }
         $result_query->data = $all;
-        $stmt->close();
+
+        $db->close();
         return $result_query;
     }
 
     public function update($db) {
         $result_query = new result_query();
         $result_query->data = "ok";
-        $query = "UPDATE scoreboard SET game_id = ?, player_id = ?, player_name = ?, score = ? WHERE id = ?";
-        $stmt = $db->prepare($query);
-        $stmt->bind_param('issii', $this->game_id, $this->player_id, $this->player_name, $this->score, $this->id);
-        $stmt->execute();
-        if ($stmt->error != ""){
-            $result_query->error = "error at update one score : ".$stmt->error;
+        $query = "UPDATE scoreboard SET game_id = $this->game_id, player_id = $this->player_id, player_name = $this->player_name, score = $this->score WHERE id = $this->id";
+        $stmt = $db->query($query);
+        if (!$stmt){
+            $result_query->error = "error at add new score : ".$stmt->error;
             $result_query->data = "not ok";
-            $stmt->close();
-            return $result_query;
+            $db->close();
         }
-        $stmt->close();
+
+        $db->close();
         return $result_query;
     }
     
     public function delete($db) {
         $result_query = new result_query();
         $result_query->data = "ok";
-        $query = "DELETE FROM scoreboard WHERE id=?";
-        $stmt = $db->prepare($query);
-        $stmt->bind_param('i', $this->id);
-        $stmt->execute();
-        if ($stmt->error != ""){
-            $result_query->error = "error at delete one score : ".$stmt->error;
+        $query = "DELETE FROM scoreboard WHERE id = $this->id";
+        $stmt = $db->query($query);
+        if (!$stmt){
+            $result_query->error = "error at add new score : ".$stmt->error;
             $result_query->data = "not ok";
-            $stmt->close();
-            return $result_query;
+            $db->close();
         }
-        $stmt->close();
+
+        $db->close();
         return $result_query;
     }
 }
